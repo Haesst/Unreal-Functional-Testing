c1955d6e9dfe4ca5abcb88714d4a38c53f81398d
Robin Ortryd <45140757+Haesst@users.noreply.github.com>
2020-08-18 11:34:53 +0200

    First init

---
+++ Source/ShooterGame/Private/Pickups/Ammo_Pickup_Test.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShooterGame.h"
+#include "Ammo_Pickup_Test.h"
+
+Ammo_Pickup_Test::Ammo_Pickup_Test()
+{
+}
+
+Ammo_Pickup_Test::~Ammo_Pickup_Test()
+{
+}
---
+++ Source/ShooterGame/Public/Pickups/Ammo_Pickup_Test.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SHOOTERGAME_API Ammo_Pickup_Test
+{
+public:
+	Ammo_Pickup_Test();
+	~Ammo_Pickup_Test();
+};
